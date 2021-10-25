05a09a847344b1561cca8bcefd021738b9b044b9
Roger Medrano <email@example.com>
2021-10-25 13:30:54 -0700

    f

---
+++ ORM_Dapper1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ORM_Dapper1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
